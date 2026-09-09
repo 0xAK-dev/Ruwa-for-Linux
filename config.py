e7fe878a60dc9794ed7c7be6c9bf756c00164ca1
0xAK-dev <akgithub009@gmail.com>
2026-09-09 22:25:48 +0200

    refactor: replace hardcoded Ruwa versions

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
-VERSIONS = [
-    "0.3.4-alpha",
-    "0.3.3-alpha",
-    "0.3.2-alpha",
-    "0.3.1-alpha",
-    "0.3.0-alpha",
-    "0.2.9-alpha",
-    "0.2.8-alpha",
-    "0.2.7-alpha",
-    "0.2.6-alpha",
-    "0.2.5-alpha",
-    "0.2.4-alpha",
-]
+from utils.releases import get_ruwa_versions
+
+# Dynamically fetch available Ruwa versions.
+VERSIONS = get_ruwa_versions()
+
+# Or use a fixed list of versions:
+# VERSIONS = [
+#     "0.3.4-alpha",
+#     "0.3.3-alpha",
+# ]
